cc8d2eb9f3511b5bb5c28101f8c5d75d84964445
swift9518 <swift9518@outlook.com>
2025-07-03 15:43:47 -0400

    deploy portfolio project

---
+++ npm/snippets/_npm-log.js
@@ -0,0 +1,44 @@
+/**
+ * @author Lyle Swift
+ * @description Handy utilities to present console messages within your npm scripts.
+ */
+export const useNpmLogger = () => {
+    /**
+     * @enum
+     */
+    const LogTypes = {
+        DEFAULT: "log_type_default",
+        ERROR: "log_type_error",
+        WARNING: "log_type_warning",
+        SKIP: "log_type_skip",
+        SUCCESS: "log_type_success",
+        SUCCESS_FINISHED: "log_type_success_finished",
+    }
+
+    /**
+     * @constant
+     * @dictionary
+     */
+    const LOG_TYPES_MAPPING = {
+        [LogTypes.DEFAULT]:             { emoji: "⬛"  },
+        [LogTypes.WARNING]:             { emoji: "⚠️"  },
+        [LogTypes.SKIP]:                { emoji: "⏭️"  },
+        [LogTypes.SUCCESS]:             { emoji: "✅"  },
+        [LogTypes.SUCCESS_FINISHED]:    { emoji: "🎉"  },
+        [LogTypes.ERROR]:               { emoji: "❌"  },
+    }
+
+    /**
+     * @param {LogTypes} type
+     * @param {String} message
+     */
+    const log = (type, message) => {
+        const logTypeData = LOG_TYPES_MAPPING[type] || LOG_TYPES_MAPPING[LogTypes.DEFAULT]
+        console.log(`${logTypeData.emoji} · ${message}`)
+    }
+
+    return {
+        LogTypes,
+        log
+    }
+}
